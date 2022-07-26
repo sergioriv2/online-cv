7825b334f14718705c57492916682bac8b2e2863
Sergio Rivera <sergiorive02@gmail.com>
2022-07-26 20:54:21 -0300

    feat(server): add 'sections' tests

--- server/tests/me.test.js
+++ server/tests/me.test.js
@@ -4,37 +4,12 @@ const Me = require("../models/me.model");
 
 const app = new Server();
 
+const { initialInformation, request, app } = require("./helpers");
+
 describe("'/me' endpoint", () => {
-  let request;
-
-  app.openConnection().then(() => {
-    request = supertest(app.getApp());
-  });
-
-  const mockInformation = {
-    names: {
-      firstName: "lorem",
-      lastName: "lorem",
-    },
-    username: "serivera",
-    password: "lorem",
-    image: "lorem",
-    resume: "lorem",
-    academic_background: [
-      {
-        institution: "lorem",
-        title: "lorem",
-        dates: {
-          start: new Date(),
-          end: new Date(),
-        },
-      },
-    ],
-  };
-
   beforeAll(async () => {
     await Me.deleteMany({});
-    const newMe = new Me(mockInformation);
+    const newMe = new Me(initialInformation);
 
     await newMe.save();
   });
