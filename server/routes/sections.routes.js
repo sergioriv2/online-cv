58f384321504c9695021fcf7d00223ca12ac2d21
Sergio Rivera <sergiorive02@gmail.com>
2022-07-27 15:13:00 -0300

    feat(server): add more tests to the 'sections' endpoint

--- server/routes/sections.routes.js
+++ server/routes/sections.routes.js
@@ -7,6 +7,8 @@ const {
   remove,
   update,
 } = require("../controllers/sections.controller");
+const { validateFields } = require("../middlewares/validateFields.middlewares");
+const { validateJWT } = require("../middlewares/validateJWT");
 
 const router = Router();
 
@@ -14,10 +16,51 @@ const router = Router();
 
 router.get("/", getAll);
 
-router.post("/", create);
+router.post(
+  "/",
+  [
+    validateJWT,
+    check("title.es", "'spanish title' field is required.")
+      .notEmpty()
+      .isString()
+      .withMessage("'spanish title' must be a string."),
+    check("title.en", "'english title' must be a string.")
+      .isString()
+      .optional(),
+    check("description.es", "'spanish description' field is required.")
+      .notEmpty()
+      .isString()
+      .withMessage("'spanish description' must be a string."),
+    check("description.en", "'english description' field is required.")
+      .notEmpty()
+      .isString()
+      .withMessage("'english description' must be a string."),
+    validateFields,
+  ],
+  create
+);
 
-router.delete("/:id", remove);
+router.delete("/:id", [validateJWT, validateFields], remove);
 
-router.put("/:id", update);
+router.put(
+  "/:id",
+  [
+    validateJWT,
+    check("title.es", "'spanish title' must be a string.")
+      .isString()
+      .optional(),
+    check("title.en", "'english title' must be a string.")
+      .isString()
+      .optional(),
+    check("description.es", "'spanish description' must be a string.")
+      .isString()
+      .optional(),
+    check("description.en", "'english description' must be a string.")
+      .isString()
+      .optional(),
+    validateFields,
+  ],
+  update
+);
 
 module.exports = router;
--- server/tests/sections.test.js
+++ server/tests/sections.test.js
@@ -38,101 +38,155 @@ describe("'/sections' endpoint", () => {
     done();
   });
 
-  it("should return the data as a json", async () => {
-    await request
-      .get("/api/sections")
-      .expect(200)
-      .expect("content-type", /application\/json/);
+  describe("* GET /api/projects", () => {
+    it("should return the data as a json", async () => {
+      await request
+        .get("/api/sections")
+        .expect(200)
+        .expect("content-type", /application\/json/);
+    });
+
+    it("should return all the sections", async () => {
+      const response = await request.get("/api/sections");
+
+      // Test length
+      expect(response.body.results).toHaveLength(initialSections.length);
+
+      // Test object returned
+      expect(response.body).toMatchObject({
+        ok: true,
+        results: expect.arrayContaining([
+          expect.objectContaining({
+            uid: expect.any(String),
+            title: expect.objectContaining({
+              es: expect.any(String),
+              en: expect.any(String),
+            }),
+            description: expect.objectContaining({
+              es: expect.any(String),
+              en: expect.any(String),
+            }),
+          }),
+        ]),
+      });
+    });
   });
 
-  it("should return all the sections", async () => {
-    const response = await request.get("/api/sections");
+  describe("* POST /api/projects", () => {
+    describe("-- Without JWT", () => {
+      it("should return an unauthorized response", async () => {
+        await request.post("/api/sections/").expect(401);
+      });
+    });
 
-    // Test length
-    expect(response.body.results).toHaveLength(initialSections.length);
+    it("should post a new section", async () => {
+      const newSection = {
+        title: {
+          es: "Nueva seccion",
+          en: "New section",
+        },
+        description: {
+          en: "<p>Welcome to the new section</p>",
+          es: "<p>Bienvenido a la nueva sección</p>",
+        },
+      };
 
-    // Test object returned
-    expect(response.body).toMatchObject({
-      ok: true,
-      results: expect.arrayContaining([
-        expect.objectContaining({
+      const updatedSection = await request
+        .post("/api/sections/")
+        .expect(201)
+        .send(newSection)
+        .set("X-Api-Key", token);
+
+      const { body: sections } = await request.get("/api/sections");
+
+      expect(updatedSection.body).toMatchObject({
+        ok: true,
+        results: expect.objectContaining({
+          title: newSection.title,
+          description: newSection.description,
           uid: expect.any(String),
-          title: expect.objectContaining({
-            es: expect.any(String),
-            en: expect.any(String),
-          }),
-          description: expect.objectContaining({
-            es: expect.any(String),
-            en: expect.any(String),
-          }),
         }),
-      ]),
+      });
+
+      // Check if the length of the collection has changed
+      expect(sections.results.length).toBe(initialSections.length + 1);
     });
   });
 
-  it("should update a section", async () => {
-    const updateSection = {
-      title: {
-        es: "Bienvenidos",
-        en: "Bienvenidos",
-      },
-      description: {
-        es: "<p>Lorem ipsum dolor sit amet</p>",
-        en: "<p>Lorem ipsum dolor sit amet</p>",
-      },
-    };
+  describe("* PUT /api/projects", () => {
+    describe("-- Without JWT", () => {
+      it("should return an unauthorized response", async () => {
+        const { body: sectionsResponse } = await request
+          .get("/api/sections/")
+          .expect(200);
+        const [sectionToUpdate] = sectionsResponse.results;
 
-    const { body: sectionsResponse } = await request
-      .get("/api/sections/")
-      .expect(200)
-      .set("X-Api-Key", token);
-    const [sectionToUpdate] = sectionsResponse.results;
+        await request.put("/api/sections/" + sectionToUpdate.uid).expect(401);
+      });
+    });
 
-    const updatedSection = await request
-      .put("/api/sections/" + sectionToUpdate.uid)
-      .send(updateSection);
+    it("should update a section", async () => {
+      const updateSection = {
+        title: {
+          es: "Bienvenidos",
+          en: "Bienvenidos",
+        },
+        description: {
+          es: "<p>Lorem ipsum dolor sit amet</p>",
+          en: "<p>Lorem ipsum dolor sit amet</p>",
+        },
+      };
 
-    expect(updatedSection.body).toMatchObject({
-      ok: true,
-      results: expect.objectContaining({
-        title: updateSection.title,
-        description: updateSection.description,
-        uid: expect.any(String),
-      }),
+      const { body: sectionsResponse } = await request
+        .get("/api/sections/")
+        .expect(200);
+      const [sectionToUpdate] = sectionsResponse.results;
+
+      const updatedSection = await request
+        .put("/api/sections/" + sectionToUpdate.uid)
+        .send(updateSection)
+        .set("X-Api-Key", token);
+
+      expect(updatedSection.body).toMatchObject({
+        ok: true,
+        results: expect.objectContaining({
+          title: updateSection.title,
+          description: updateSection.description,
+          uid: expect.any(String),
+        }),
+      });
     });
   });
 
-  it("should post a new section", async () => {
-    const newSection = {
-      title: {
-        es: "Nueva seccion",
-        en: "New section",
-      },
-      description: {
-        en: "<p>Welcome to the new section</p>",
-        es: "<p>Bienvenido a la nueva sección</p>",
-      },
-    };
+  describe("* DELETE /api/projects", () => {
+    describe("-- Without JWT", () => {
+      it("should return an unauthorized response", async () => {
+        const { body: sectionsResponse } = await request
+          .get("/api/sections/")
+          .expect(200);
+        const [sectionToDelete] = sectionsResponse.results;
 
-    const updatedSection = await request
-      .post("/api/sections/")
-      .expect(201)
-      .send(newSection)
-      .set("X-Api-Key", token);
-
-    const { body: sections } = await request.get("/api/sections");
-
-    expect(updatedSection.body).toMatchObject({
-      ok: true,
-      results: expect.objectContaining({
-        title: newSection.title,
-        description: newSection.description,
-        uid: expect.any(String),
-      }),
+        await request
+          .delete("/api/sections/" + sectionToDelete.uid)
+          .expect(401);
+      });
     });
 
-    // Check if the length of the collection has changed
-    expect(sections.results.length).toBe(initialSections.length + 1);
+    it("should delete a section", async () => {
+      const { body: sectionsResponse } = await request
+        .get("/api/sections/")
+        .expect(200);
+      const [sectionToDelete] = sectionsResponse.results;
+
+      const response = await request
+        .delete("/api/sections/" + sectionToDelete.uid)
+        .set("X-Api-Key", token);
+
+      expect(response.body).toMatchObject({
+        ok: true,
+        msg: "Deleted successfully.",
+      });
+    });
   });
 
   afterAll(async () => {
